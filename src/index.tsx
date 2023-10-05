2657a3e896b312ca1de7ae197a1fc76d81aaa972
giuliabferreira <giuliabferreira@hotmail.com>
2023-10-05 08:59:37 -0300

    .

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// 1 - configurando router
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from './routes/Home';
+import Contact from './routes/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+
+  {
+    path: 'contact',
+    element: <Contact />
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
